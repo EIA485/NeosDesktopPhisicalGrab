c6342d95258f64c56549b71aa990c0c31daaa63e
EIA485 <17285570+EIA485@users.noreply.github.com>
2022-01-29 15:54:34 -0800

    Add project files.

---
+++ DesktopPhisicalGrab/DesktopPhisicalGrab.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+using System;
+using System.Reflection;
+
+namespace DesktopPhisicalGrab
+{
+    public class DesktopPhisicalGrab : NeosMod
+    {
+        public override string Name => "DesktopPhisicalGrab";
+        public override string Author => "eia485";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/eia485/NeosDesktopPhisicalGrab/";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.eia485.DesktopPhisicalGrab");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(CommonTool), "TryPointGrab")]
+        class DesktopPhisicalGrabPatch
+        {
+            static bool Prefix(CommonTool __instance)
+            {
+                return !(bool)typeof(CommonTool).GetMethod("Grab", BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[] { typeof(bool), typeof(ICollider) }, null).Invoke(__instance, new object[] { false, null });
+            }
+        }
+    }
+}
